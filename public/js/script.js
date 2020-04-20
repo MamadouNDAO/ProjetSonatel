0889520a9cea3cb64d3ebb375193d7700eff0bd5
MamadouNDAO <itech.ndao@gmail.com>
2020-04-20 06:33:53 +0000

    Commit 3

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,7 @@
 $(document).ready(function(){
     $("#liqst").click(function(){
-            $(this).attr('src','./public/images/ic-liste.png');  
-          },
-          function () {
-             $(this).attr('src','./public/images/ic-liste-active.png');  
-          }
-
-
-
-
-
-        //$("#liqst").css({"background-color":"rgb(234, 235, 236)",
-        // "border-left": "solid 5px green"});
+        $("#liqst").css({"background-color":"rgb(234, 235, 236)",
+         "border-left": "solid 5px green"});
 
 
 });
